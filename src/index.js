e7c3035ee14e1fa9c389815505b10b736dc7552a
ivarmlee1 <ivarr5@gmail.com>
2017-06-14 16:22:18 -0700

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, err => {
+	if (err) {
+		throw err;
+	} else {
+		// console.log(
+		// 	`server running on ${PORT}
+		// 	-----
+		// 	running on ${process.env.NODE_ENV}
+		// 	----- 
+		// `)
+	}
+});
